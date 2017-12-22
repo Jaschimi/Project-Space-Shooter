2b608b39bc050c209e918a6d97b599b1cf863bf8
Jaschimi <34771820+Jaschimi@users.noreply.github.com>
2017-12-22 16:33:56 +0100

    Uploaded the java files :-)

---
+++ SpaceShooter.java
@@ -0,0 +1,20 @@
+package objects;
+
+public class SpaceShooter extends Spaceship{
+
+	public SpaceShooter(int[] topLeftCorner, int length, int height, int lifes, boolean operating) {
+		
+		super(topLeftCorner, 3, 2, 3, true);
+	}
+
+	public static void shoot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Spaceship.java
@@ -0,0 +1,16 @@
+package objects;
+
+public abstract class Spaceship extends ColoringField {
+
+	public Spaceship(int[] topLeftCorner, int length, int height, int lifes, boolean operating){
+		
+		super(length, height);
+		
+	}
+	
+	public static void shoot() {
+	}
+	
+	public static void move() {
+	}
+}
